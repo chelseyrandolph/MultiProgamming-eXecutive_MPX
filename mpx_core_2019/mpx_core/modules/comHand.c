d0291a551ec150bdbfc0e192fae64da7252be02f
James Jackman <jtj0002@mix.wvu.edu>
2019-09-08 14:47:32 -0400

    command handler almost complete, still error in kmain

--- mpx_core_2019/mpx_core/modules/comHand.c
+++ mpx_core_2019/mpx_core/modules/comHand.c
@@ -5,80 +5,88 @@
 #include "mpx_supt.h"
 #include "comhand.h"
 
+void version();
+void ourHelp();
+int shutDown();
+void getTime();
+void getDate();
+void setTime();
+void setDate();
 
-int comHandler(const char * buffer){
+
+int comHandler(){
 	char cmdBuffer[100];
 	int bufferSize;
 	int quit = 0;
-	int menuSize = 2000;
-	char menu[menuSize] = {
-		"__________________________________________________________"
-		" ____ ___                                                 "
-		"|    |   \______ ___________  ____ _____    _____   ____  "
-		"|    |   /  ___// __ \_  __ \/     \__  \  /     \_/ __ \ "
-		"|    |  /\___  \  ___/|  | \/   |  \/ __ \|  Y Y  \  ___/ "
-		"|______//____  >\___  >__|  |___|  (____  /__|_|  /\___  >"
-		"             \/     \/           \/     \/      \/     \/ "
-		"		   _____                       
-		"		  /     \   ____   ____  __ __ 		   "
-		"		 /  \ /  \_/ __ \ /    \|  |  \		   "
-		"		/    Y    \  ___/|   |  \  |  /		   "
-		"		\____|__  /\___  >___|  /____/ 		   "
-		"			\/     \/     \/       		   "
-		"\n	Press Corresponding number to execute command	   "
-		"	1. Version					   "
-		"	2. Help						   "
-		"	3. Shutdown					   "
-		"	4. Get time					   "
-		"	5. Get Date					   "
-		"	6. Set Time					   "
-		"	7. Set Date					   "
-		"__________________________________________________________"
-	};
+	int menuSize = 2500;
+	char menu[2500] = {"\nPress Corresponding number to execute command \n 1. Version \n 2. Help \n 3. Shutdown \n 4. Get time \n 5. Get Date \n 6. Set Time \n 7. Set Date"};
 	sys_req(WRITE,DEFAULT_DEVICE, menu, &menuSize);
 
 	while(!quit){
 		//get a command
-		memset(buffer, "\0", 100);
+		memset(cmdBuffer, '\0', 100);
 		bufferSize = 99;
 		sys_req(READ,DEFAULT_DEVICE,cmdBuffer,&bufferSize);
 		int failSize = 100;
-		char failure[failSize] = "Not a valid command, please enter a valid number";
-		
-		switch(cmBuffer){
+		char failure[100] = "Not a valid command, please enter a valid number";
+		int i = atoi(cmdBuffer);
+		switch(i){
 			case 1: version(); 					break;
-			case 2: help();						break;
-			case 3: shutDown();					break;
+			case 2: ourHelp();					break;
+			case 3: quit = shutDown();				break;
 			case 4: getTime();					break;
 			case 5: getDate();					break;
 			case 6: setTime();					break;
-			case 7:	setDate();					break;
-			default: sys_req(WRITE,DEFAULT, failure, &failSize);
+			case 7: setDate();					break;
+			default: sys_req(WRITE,DEFAULT_DEVICE, failure, &failSize);
 		}
 	}
+	return 0;
 }
 
 void version(){
-	double version = 1.0;
-	sys_req(WRITE,DEFAULT_DEVICE, version, &version);
+	int verSize = 10;
+	char version[10] = "1.0";
+	sys_req(WRITE,DEFAULT_DEVICE, version, &verSize);
 }
+
+void ourHelp(){}
 
 
 int shutDown(){
 	int promptInt = 50;
-	char prompt[promptInt] = "Are you sure? \n 1. Yes \n 2. No"
+	char prompt[50] = "Are you sure? \n 1. Yes \n 2. No";
 	int ansInt;
 	char ans[10];
 
 	sys_req(WRITE, DEFAULT_DEVICE, prompt, &promptInt);
-	sys_req(READ, DEFAULT_DEVICE, ans , ansInt
-	if(ans = "1"){
+	sys_req(READ, DEFAULT_DEVICE, ans , &ansInt);
+	int i = atoi(ans);
+	if(i){
 		return 1;
 	}
 	else{
 		return 0;
 	}
 }
+
+void getTime(){}
+void getDate(){}
+void setTime(){}
+void setDate(){}
+
+
+//		" ____ ___                                                 "                    
+//		"|    |   \______ ___________  ____ _____    _____   ____  "
+//		"|    |   /  ___// __\\_  __\\/    \\__ \\  /    \\_/ __\\ "
+//		"|    |  /\\___\\  ___/|  |\\/   | \\/ __\\|  Y Y \\  ___/ "
+//		"|______//____ >\\___  >__|  |___|  (____  /__|_|  /\\___ >"
+//		"            \\/    \\/          \\/    \\/     \\/    \\/ 
+//		"		  /    \\   ____   ____  __ __ 		   "
+//		"		 /  \ / \\_/ __\\ /   \\|  | \\		   "
+//		"		/    Y   \\  ___/|   | \\  |  /		   "
+//		"	       \\____|__ /\\___  >___|  /____/ 		   "
+//		"		       \\/    \\/    \\/       		   "
 
 
 
