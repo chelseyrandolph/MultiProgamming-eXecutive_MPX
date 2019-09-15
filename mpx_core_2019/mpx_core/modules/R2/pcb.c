d5fb49dd06e0f1df3bae81aaf245b0065a85301b
Brandon <bas0007@mix.wvu.edu>
2019-09-15 11:59:02 -0400

    progress pcb.c

--- mpx_core_2019/mpx_core/modules/R2/pcb.c
+++ mpx_core_2019/mpx_core/modules/R2/pcb.c
@@ -30,16 +30,20 @@ PCB* setup_pcb(char *name, int pclass, int priority){ // pclass refers to proces
 	if(strlen(name) >= 8){	// Process name must be at least 8 characters
 		strcpy(new_pcb -> name , name);		// sets the new process's name to name parameter
 	}else{
+		klogv("ERROR in setup_pcb(): invalid name");
 		return NULL; // TODO ERROR CODE
 	}
 	if(priority >= 0 && priority <= 9){		// priority must be a number between 0 and 9
 		new_pcb -> priority = priority;		// sets the new process's priority to priority parameter
 	}else{
+		klogv("ERROR in setup_pcb(): invalid priority");
 		return NULL; // TODO ERROR CODE
 	}
+	pclass = 1;
 	if(pclass == 0 || pclass ==1){			// process class must be either 0 for system, or 1 for user process
 		new_pcb -> process_class = pclass;	// sets the new process's class to the pclass parameter
 	}else{
+		klogv("ERROR in setup_pcb(): invalid class");
 		return NULL; // TODO ERROR CODE
 	}
 	new_pcb->readystate = 0; //ready
@@ -101,87 +105,19 @@ PCB* find_pcb(char *process_name){
 
 
 int insert_pcb(PCB *pcb){
-	if(pcb->suspended == 0){	//checks to make sure PCB is not suspended
-		if(pcb->readystate == -1){ //If ready state is blocked (readystate of -1 means blocked)
-			pcb->prev = blocked_queue.head;  //adds PCB to end of queue if blocked because blocked queue is FIFO
-			blocked_queue.head = pcb;
-			if(blocked_queue.count == 0){
-				blocked_queue.tail = pcb;
-
-			}
-			blocked_queue.count++;	// increment blocked_queue count
-		}else if(pcb->readystate == 0){ //If ready state is ready (readystate of 0 means ready)
-			int found = 0;
-			PCB *temp_pcb = ready_queue.tail;	//set temp PCB to tail of ready_queue
-			while(!found){	//for ready_queue, PCBs are inserted in order of priority 
-				
-				if(ready_queue.count == 0){
-					ready_queue.head = pcb;
-					ready_queue.tail = pcb;
-					ready_queue.count++;
-					found = 1;
-				}else if(ready_queue.count == 1){
-					if(temp_pcb->priority >= pcb->priority){
-						ready_queue.tail = temp_pcb;
-						temp_pcb->next = pcb;
-						pcb->prev = temp_pcb;
-						found = 1;
-						ready_queue.count++;
-					}else{
-						ready_queue.head = temp_pcb;
-						temp_pcb->prev = pcb;
-						pcb->next = temp_pcb;
-						found = 1;
-						ready_queue.count++;
-					}
-
-				}else if(pcb->priority == temp_pcb->priority){	
-					klogv("1");
-					pcb->next = temp_pcb;	// There two lines insert the PCB into the linked list
-					temp_pcb->prev->next = pcb;
-					temp_pcb->prev = pcb;
-					ready_queue.count++;
-					found = 1;
-				}else if(temp_pcb->next == ready_queue.head){ //if the pcb is the highest priority, make it the new head of ready_queue
-					klogv("2");
-					ready_queue.head->next = pcb; 
-					pcb->prev = ready_queue.head;
-					ready_queue.head = pcb;
-					ready_queue.count++;
-					found = 1;
-				}else{
-					klogv("3");
-					temp_pcb = temp_pcb->next;	//moves onto next PCB in linked list
-				}
-			}
-		}
-	}else{						//If the PCB IS suspended, start here
-		if(pcb->readystate == -1){ //If ready state is blocked (readystate of -1 means blocked)
-			pcb->prev = blocked_queue.head;  //adds PCB to end of queue if blocked because blocked queue is FIFO
-			blocked_queue.head = pcb;
-			suspended_blocked_queue.count++;	// increment blocked_queue count
-		}else if(pcb->readystate == 0){ //If ready state is ready (readystate of 0 means ready)
-			int found = 0;
-			PCB *temp_pcb = suspended_ready_queue.tail;	//set temp PCB to tail of ready_queue
-			while(!found){	//for ready_queue, PCBs are inserted in order of priority 
-				if(suspended_ready_queue.count == 0){
-					suspended_ready_queue.head = pcb;
-					suspended_ready_queue.tail = pcb;
-					suspended_ready_queue.count++;
-					found = 1;
-				}else if(pcb->priority == temp_pcb->priority){	
-					pcb->next = temp_pcb;	// There two lines insert the PCB into the linked list
-					temp_pcb->prev->next = pcb;
-					found = 1;
-				}else if(temp_pcb->next == suspended_ready_queue.head){ //if the pcb is the highest priority, make it the new head of ready_queue
-					suspended_ready_queue.head->next = pcb; 
-					pcb->prev = suspended_ready_queue.head;
-					suspended_ready_queue.head = pcb;
-					found = 1;
-				}else{
-					temp_pcb = temp_pcb->next;	//moves onto next PCB in linked list
-				}
-			}
+	if(pcb->readystate == 0 && pcb->suspended == 0){
+		if(ready_queue.count == 0){
+			ready_queue.head = pcb;
+			ready_queue.tail = pcb;
+			ready_queue.count++;
+			//klogv("inserted 1st pcb");
+		}else{
+			PCB *temp_pcb = ready_queue.head;
+			temp_pcb->prev = pcb;
+			pcb->next = temp_pcb;
+			ready_queue.head = pcb;
+			ready_queue.count++;
+			//klogv("inserted a pcb");
 		}
 	}
 	return 0;
@@ -339,9 +275,9 @@ int set_pcb_priority(char name[30], int new_priority){
 	return 0;
 }
 
-int show_pcb(char name[30]){
+int show_pcb(char name[16]){
 	PCB *pcb = find_pcb(name);
-	int name_size = sizeof(pcb->name);
+	int name_size = 16;
 	char *pclass;
 	char *readystate_str;
 	char *suspended_str;
@@ -387,13 +323,18 @@ int show_pcb(char name[30]){
 int show_ready(){
 	
 	int done = 0;
-	PCB *pcb = ready_queue.tail;
+	PCB *pcb = ready_queue.head;
 	while(!done){
 		show_pcb(pcb->name);
-		if(pcb == ready_queue.head){
+		//int size = 30;
+		//sys_req(WRITE, DEFAULT_DEVICE, pcb->name, &size);
+
+		if(pcb == ready_queue.tail){
 			done = 1;
+			//klogv("show_ready()   done = 1");
 		}else {
 			pcb = pcb->next;
+			//klogv("show_ready()   pcb = pcb->next");
 		}
 	}	
 	done = 0;
