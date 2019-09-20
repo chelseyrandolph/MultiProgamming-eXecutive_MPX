c0e65bf5cac3154b7a64a352b12b53594a9fee12
chelseyrando <crr0002@mix.wvu.edu>
2019-09-20 13:40:35 -0400

    pcb update

--- mpx_core_2019/mpx_core/modules/R2/pcb.c
+++ mpx_core_2019/mpx_core/modules/R2/pcb.c
@@ -1,8 +1,10 @@
 
 #include <string.h>
-
+#include <system.h>
+#include <core/io.h>
+#include <core/serial.h>
 #include "pcb.h" 
-#include "../mpx_supt.h"
+#include "mpx_supt.h"
 
 
 queue ready_queue = {.head = NULL, .tail = NULL, .count = 0};
@@ -14,24 +16,34 @@ PCB* allocate_pcb(){
 	
 	PCB *pcb = sys_alloc_mem(sizeof(pcb));
 	
+	// set up memory
+	pcb -> bottom_of_stack = sys_alloc_mem(1024);
+	//pcb -> top_of_stack = pcb -> bottom_of_stack - sizeof(CONTENT) + 1024;
+	//pcb -> content = (CONTENT*) pcb -> top_of_stack;
 	return pcb;
 }
 
-PCB* setup_pcb(char *name, int pclass, int priority){
+PCB* setup_pcb(char *name, int pclass, int priority){ // pclass refers to process class (system or user)
 
 	PCB *new_pcb = allocate_pcb();		// allocates memory to the new PCB
-
-	strcpy(new_pcb -> name , name);		// sets the new process's name to name parameter
-	new_pcb -> priority = priority;		// sets the new process's priority to priority parameter
-	new_pcb -> process_class = pclass;	// sets the new process's class to the pclass parameter
 	
-	/*
-		TODO - check each param 
-		* pclass either 0 or 1
-		* name is a valid str with length AT LEAST 8
-		* priority between 0 and 9
-		* return ERROR MESSAGES
-	*/
+	if(strlen(name) >= 8){	// Process name must be at least 8 characters
+		strcpy(new_pcb -> name , name);		// sets the new process's name to name parameter
+	}else{
+		return NULL; // TODO ERROR CODE
+	}
+	if(priority >= 0 && priority <= 9){		// priority must be a number between 0 and 9
+		new_pcb -> priority = priority;		// sets the new process's priority to priority parameter
+	}else{
+		return NULL; // TODO ERROR CODE
+	}
+	if(pclass == 0 || pclass ==1){			// process class must be either 0 for system, or 1 for user process
+		new_pcb -> process_class = pclass;	// sets the new process's class to the pclass parameter
+	}else{
+		return NULL; // TODO ERROR CODE
+	}
+	new_pcb->readystate = 0; //ready
+	new_pcb->suspended = 0;		//not suspended
 	return new_pcb;
 }
 
@@ -89,26 +101,56 @@ PCB* find_pcb(char *process_name){
 
 
 int insert_pcb(PCB *pcb){
-
 	if(pcb->suspended == 0){	//checks to make sure PCB is not suspended
 		if(pcb->readystate == -1){ //If ready state is blocked (readystate of -1 means blocked)
 			pcb->prev = blocked_queue.head;  //adds PCB to end of queue if blocked because blocked queue is FIFO
 			blocked_queue.head = pcb;
+			if(blocked_queue.count == 0){
+				blocked_queue.tail = pcb;
+
+			}
 			blocked_queue.count++;	// increment blocked_queue count
 		}else if(pcb->readystate == 0){ //If ready state is ready (readystate of 0 means ready)
 			int found = 0;
 			PCB *temp_pcb = ready_queue.tail;	//set temp PCB to tail of ready_queue
 			while(!found){	//for ready_queue, PCBs are inserted in order of priority 
-				if(pcb->priority == temp_pcb->priority){	
+				
+				if(ready_queue.count == 0){
+					ready_queue.head = pcb;
+					ready_queue.tail = pcb;
+					ready_queue.count++;
+					found = 1;
+				}else if(ready_queue.count == 1){
+					if(temp_pcb->priority >= pcb->priority){
+						ready_queue.tail = temp_pcb;
+						temp_pcb->next = pcb;
+						pcb->prev = temp_pcb;
+						found = 1;
+						ready_queue.count++;
+					}else{
+						ready_queue.head = temp_pcb;
+						temp_pcb->prev = pcb;
+						pcb->next = temp_pcb;
+						found = 1;
+						ready_queue.count++;
+					}
+
+				}else if(pcb->priority == temp_pcb->priority){	
+					klogv("1");
 					pcb->next = temp_pcb;	// There two lines insert the PCB into the linked list
 					temp_pcb->prev->next = pcb;
+					temp_pcb->prev = pcb;
+					ready_queue.count++;
 					found = 1;
 				}else if(temp_pcb->next == ready_queue.head){ //if the pcb is the highest priority, make it the new head of ready_queue
+					klogv("2");
 					ready_queue.head->next = pcb; 
 					pcb->prev = ready_queue.head;
 					ready_queue.head = pcb;
+					ready_queue.count++;
 					found = 1;
 				}else{
+					klogv("3");
 					temp_pcb = temp_pcb->next;	//moves onto next PCB in linked list
 				}
 			}
@@ -122,7 +164,12 @@ int insert_pcb(PCB *pcb){
 			int found = 0;
 			PCB *temp_pcb = suspended_ready_queue.tail;	//set temp PCB to tail of ready_queue
 			while(!found){	//for ready_queue, PCBs are inserted in order of priority 
-				if(pcb->priority == temp_pcb->priority){	
+				if(suspended_ready_queue.count == 0){
+					suspended_ready_queue.head = pcb;
+					suspended_ready_queue.tail = pcb;
+					suspended_ready_queue.count++;
+					found = 1;
+				}else if(pcb->priority == temp_pcb->priority){	
 					pcb->next = temp_pcb;	// There two lines insert the PCB into the linked list
 					temp_pcb->prev->next = pcb;
 					found = 1;
@@ -139,6 +186,264 @@ int insert_pcb(PCB *pcb){
 	}
 	return 0;
 }
+
+int remove_pcb(PCB* pcb){
+	if(pcb->readystate == 0 && pcb->suspended == 0){	//ready
+		PCB* temp_pcb = ready_queue.tail;
+		int found = 0;
+		while(!found){
+			if(pcb == temp_pcb){
+				temp_pcb->next->prev = temp_pcb->prev;
+				temp_pcb->prev->next = temp_pcb->next;
+			}else if(temp_pcb == ready_queue.head){
+				return 0; // TODO ERROR CODE  PCB NOT FOUND
+			}else{
+				temp_pcb = temp_pcb->next;
+			}
+		}
+	}
+	else if(pcb->readystate == 0 && pcb->suspended == 1){ //suspended ready
+		PCB* temp_pcb = suspended_ready_queue.tail;
+		int found = 0;
+		while(!found){
+			if(pcb == temp_pcb){
+				temp_pcb->next->prev = temp_pcb->prev;
+				temp_pcb->prev->next = temp_pcb->next;
+			}else if(temp_pcb == suspended_ready_queue.head){
+				return 0; // TODO ERROR CODE  PCB NOT FOUND
+			}else{
+				temp_pcb = temp_pcb->next;
+			}
+		}
+	}
+	else if(pcb->readystate == -1 && pcb->suspended == 0){ //blocked
+		PCB* temp_pcb = blocked_queue.tail;
+		int found = 0;
+		while(!found){
+			if(pcb == temp_pcb){
+				temp_pcb->next->prev = temp_pcb->prev;
+				temp_pcb->prev->next = temp_pcb->next;
+			}else if(temp_pcb == blocked_queue.head){
+				return 0; // TODO ERROR CODE  PCB NOT FOUND
+			}else{
+				temp_pcb = temp_pcb->next;
+			}
+		}		
+	}
+	else if(pcb->readystate == -1 && pcb->suspended == 1){ //suspended blocked
+		PCB* temp_pcb = suspended_blocked_queue.tail;
+		int found = 0;
+		while(!found){
+			if(pcb == temp_pcb){
+				temp_pcb->next->prev = temp_pcb->prev;
+				temp_pcb->prev->next = temp_pcb->next;
+			}else if(temp_pcb == suspended_blocked_queue.head){
+				return 0; // TODO ERROR CODE  PCB NOT FOUND
+			}else{
+				temp_pcb = temp_pcb->next;
+			}
+		}		
+	}	
+	else{	// PROCESS NOT FOUND
+
+		return 0; // TODO ERROR CODE
+	}
+	return 0;
+}
+
+/*========================= USER COMMANDS ========================*/
+
+int create_pcb(){ 
+	char name[30];
+	char priority_str[2];
+	char pclass_str[2]; // pclass refers to process class (system or user)
+	char name_prompt[40] = "Please Enter a name for the process:	";
+	char pclass_prompt[100] = "Please Enter the class [ 0 for system process, 1 for user process]:	";
+	char priority_prompt[100] = "Please Enter the priority [ 0 being the lowest, and 9 being the highest]:	";
+
+	int prompt_size = 40;
+	int prompt_size2 = 100;
+
+	sys_req(WRITE, DEFAULT_DEVICE, name_prompt, &prompt_size);
+	sys_req(READ, DEFAULT_DEVICE, name, &prompt_size);
+	sys_req(WRITE, DEFAULT_DEVICE, pclass_prompt, &prompt_size2);
+	sys_req(READ, DEFAULT_DEVICE, pclass_str, &prompt_size);
+	sys_req(WRITE, DEFAULT_DEVICE, priority_prompt, &prompt_size2);
+	sys_req(READ, DEFAULT_DEVICE, priority_str, &prompt_size);
+	
+	int pclass = atoi(pclass_str);
+	int priority = atoi(priority_str);
+
+	if(strlen(name) < 8){	// Process name must be at least 8 characters
+		klogv("invalid Name... exiting");
+		return NULL; // TODO ERROR CODE
+	}
+	if(priority < 0 || priority > 9){		// priority must be a number between 0 and 9
+		klogv("invalid priority... exiting");
+		return NULL;	// TODO ERROR CODE
+	}
+	if(!pclass == 0 && !pclass ==1){		// process class must be either 0 for system, or 1 for user process
+		klogv("invalid process class... exiting");
+		return NULL; 	//TODO ERROR CODE
+	}
+	insert_pcb(setup_pcb(name, pclass, priority)); // Insert a setup PCB (insert_pcb() takes are of placing it in the appropriate queue
+	return 0;
+}
+
+// TODO FOR ALL USER FUNCTIONS -----  ERROR CHECKING ERROR CHECKING ERROR CHECKING
+
+int delete_pcb(char name[30]){
+	PCB *pcb = find_pcb(name);
+	remove_pcb(pcb);
+	free_pcb(pcb);
+	return 0;
+}
+
+int block_pcb(char name[30]){
+	PCB *pcb = find_pcb(name);
+	remove_pcb(pcb);		//takes the pcb out of the queue its in
+	pcb->readystate = -1;	//sets readystate = blocked
+	insert_pcb(pcb);			//inserts it into the blocked queue
+	return 0;
+}
+
+int unblock_pcb(char name[30]){
+	PCB *pcb = find_pcb(name);
+	remove_pcb(pcb);		//takes the pcb out of the queue its in
+	pcb->readystate = 0;	//sets readystate = ready
+	insert_pcb(pcb);			//inserts it into the ready (or suspended_ready) queue
+	return 0;
+}
+
+int suspend_pcb(char name[30]){
+	PCB *pcb = find_pcb(name);
+	remove_pcb(pcb);		//takes the pcb out of the queue its in
+	pcb->suspended = 1;	//sets suspended = suspended
+	insert_pcb(pcb);		//inserts it into the appropriate suspended queue
+	return 0;
+}
+
+int resume_pcb(char name[30]){
+	PCB *pcb = find_pcb(name);
+	remove_pcb(pcb);		//takes the pcb out of the queue its in
+	pcb->suspended = 0;	//sets suspended = unsuspended
+	insert_pcb(pcb);		//inserts it into the appropriate suspended queue
+	return 0;
+}
+
+int set_pcb_priority(char name[30], int new_priority){
+	PCB *pcb = find_pcb(name);
+	pcb->priority = new_priority;
+	remove_pcb(pcb);
+	insert_pcb(pcb);
+	return 0;
+}
+
+int show_pcb(char name[30]){
+	PCB *pcb = find_pcb(name);
+	int name_size = sizeof(pcb->name);
+	char *pclass;
+	char *readystate_str;
+	char *suspended_str;
+	char *vertline = " |";
+	char *newline = " \n";
+	int two = 2;
+
+
+	if(pcb->process_class == 1){
+		pclass = " User Process   |";
+	}else if(pcb->process_class == 0){
+		pclass = " System Process |";
+	}else{
+		return 0; // TODO ERROR CODE
+	}
+	if(pcb->readystate == 1){
+		readystate_str = " Running |";
+	}else if(pcb->readystate == 0){
+		readystate_str = " Ready   |";
+	}else if(pcb->readystate == -1){
+		readystate_str = " Blocked |";
+	}else{
+		return 0; // TODO ERROR CODE
+	}
+	if(pcb->suspended == 1){
+		suspended_str = " SUSPENDED     |";
+	}else if(pcb->suspended == 0){
+		suspended_str = " not suspended |";
+	}else{
+		return 0; // TODO ERROR CODE
+	}
+
+	sys_req(WRITE, DEFAULT_DEVICE, pcb->name, &name_size);
+	sys_req(WRITE, DEFAULT_DEVICE, vertline, &two);
+	sys_req(WRITE, DEFAULT_DEVICE, pclass, &name_size);
+	sys_req(WRITE, DEFAULT_DEVICE, readystate_str, &name_size);
+	sys_req(WRITE, DEFAULT_DEVICE, suspended_str, &name_size);
+	sys_req(WRITE, DEFAULT_DEVICE, (char*)pcb->priority, &name_size);
+	sys_req(WRITE, DEFAULT_DEVICE, newline, &two);
+	return 1;
+}
+
+int show_ready(){
+	
+	int done = 0;
+	PCB *pcb = ready_queue.tail;
+	while(!done){
+		show_pcb(pcb->name);
+		if(pcb == ready_queue.head){
+			done = 1;
+		}else {
+			pcb = pcb->next;
+		}
+	}	
+	done = 0;
+	pcb = suspended_ready_queue.tail;
+	while(!done){
+		show_pcb(pcb->name);
+		if(pcb == suspended_ready_queue.head){
+			done = 1;
+		}else {
+			pcb = pcb->next;
+		}
+	}
+	return 0;
+}
+
+int show_blocked(){
+	int done = 0;
+	PCB *pcb = blocked_queue.tail;
+	while(!done){
+		show_pcb(pcb->name);
+		if(pcb == blocked_queue.head){
+			done = 1;
+			show_pcb(pcb->name);
+		}else {
+			pcb = pcb->next;
+		}
+	}	
+	done = 0;
+	pcb = suspended_blocked_queue.tail;
+	while(!done){
+		show_pcb(pcb->name);
+		if(pcb == suspended_blocked_queue.head){
+			done = 1;
+		}else {
+			pcb = pcb->next;
+		}
+	}
+	return 0;
+}
+
+int show_all(){
+	show_ready();
+	show_blocked();
+	return 0;
+}
+
+
+
+
+
 
 
 
--- mpx_core_2019/mpx_core/modules/R2/pcb.h
+++ mpx_core_2019/mpx_core/modules/R2/pcb.h
@@ -9,11 +9,45 @@ typedef struct PCB {
 	struct PCB *next;		//Next PCB in linked list
 	struct PCB *prev;		//Previous PCB in linked list
 	
-	unsigned char stack[1024];
-	unsigned char *top_of_stack;
+	unsigned char* bottom_of_stack;
+	unsigned char* top_of_stack;
+	struct content* content;
 	int stack_size; 
 }PCB;
 
+PCB* allocate_pcb();
+
+PCB* setup_pcb(char *name, int pclass, int priority);
+
+int free_pcb(PCB* pcb);
+
+PCB* find_pcb(char *process_name);
+
+int insert_pcb(PCB* pcb);
+
+int remove_pcb(PCB* pcb);
+
+int create_pcb(); // may need to add parameters... for now we get the data from user input within the function
+
+int delete_pcb(char name[30]);
+
+int block_pcb(char name[30]);
+
+int unblock_pcb(char name[30]);
+
+int suspend_pcb(char name[30]);
+
+int resume_pcb(char name[30]);
+
+int show_pcb(char name[30]);
+
+int show_blocked();
+
+int show_ready();
+
+int show_all();
+
+int set_pcb_priority(char name[30], int new_priority);
 
 typedef struct queue {
 	PCB *head;
